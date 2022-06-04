4a010e3ebfd9c942e8d8e97ce62e9537bcfc9f33
berkagmpp <berkagmpp@gmail.com>
2022-06-04 17:42:00 +1200

    Feat: Add DUMMY_QUOTES, Import QuiteList in AllQuotes.js Add DUMMY_QUOTES for testing in AllQuotes.js Import QuiteList for mapping data in AllQuotes.js

--- src/components/pages/AllQuotes.js
+++ src/components/pages/AllQuotes.js
@@ -1,6 +1,21 @@
+import QuoteList from '../quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+    { 
+        id: 'q1',
+        author: 'Claire',
+        text: 'React-router practice'
+    },
+    { 
+        id: 'q2',
+        author: 'Derek',
+        text: 'React practice'
+    },
+];
+
 const AllQuotes = () => {
     return (
-        <h1>All Quotes Page!</h1>
+        <QuoteList quotes={DUMMY_QUOTES} />
     );
 };
 
