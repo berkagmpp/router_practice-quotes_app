f271abd0808cfa1688521c90907798deafe2c291
berkagmpp <berkagmpp@gmail.com>
2022-06-04 18:17:54 +1200

    Feat: Add DUMMY_QUOTES, HighlightedQuote in QuoteDetail.js Add HighlightedQuote.js for displaying a highlighted detail quote in QuoteDetail.js Add DUMMY_QUOTES for testing a highlighted detail quote in QuoteDetail.js Add find Array fn for seeking taken data from prams and HighlighedQuote

--- src/components/pages/QuoteDetail.js
+++ src/components/pages/QuoteDetail.js
@@ -1,15 +1,32 @@
 import { Fragment } from "react";
 import { useParams, Route } from "react-router-dom";
 
+import HighlightedQuote from "../quotes/HighlightedQuote";
 import Comments from '../comments/Comments'
+
+const DUMMY_QUOTES = [
+    { 
+        id: 'q1',
+        author: 'Claire',
+        text: 'React-router practice'
+    },
+    { 
+        id: 'q2',
+        author: 'Derek',
+        text: 'React practice'
+    },
+];
 
 const QuoteDetail = () => {
     const params = useParams();
 
+    const quote = DUMMY_QUOTES.find(
+        (quote) => quote.id === params.quoteId
+    );
+
     return (
         <Fragment>
-            <h1>Quote Detail Page!</h1>
-            <p>{params.quoteId}</p>
+            <HighlightedQuote author={quote.author} text={quote.text} />
             <Route path={`/quotes/${params.quoteId}/comments`}>
                 <Comments />
             </Route>
