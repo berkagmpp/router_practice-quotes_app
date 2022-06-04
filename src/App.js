12dfe01f4348279b17c88f7c2fdb0ec6b59a28b3
berkagmpp <berkagmpp@gmail.com>
2022-06-04 17:07:54 +1200

    Feat: Add MainNavigation.js Add MainNavigation.js with NavLink(react-router-dom), activeClassName Import MainNavigation.js, Fragment(react) in App.js Ref: #26b4dfe591b97e2acdf3d9301e3e708b9d07e0f0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import { Fragment } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 
+import MainNavigation from "./components/layout/MainNavigation";
 import AllQuotes from "./components/pages/AllQuotes";
 import QuoteDetail from "./components/pages/QuoteDetail";
 import NewQuote from "./components/pages/NewQuote";
 
 function App() {
     return (
-        <Switch>
-            <Route path="/" exact>
-                <Redirect to="/quotes" />
-            </Route>
-            <Route path="/quotes" exact>
-                <AllQuotes />
-            </Route>
-            <Route path="/quotes/:quoteId">
-                <QuoteDetail />
-            </Route>
-            <Route path="/new-quote">
-                <NewQuote />
-            </Route>
-        </Switch>
+        <Fragment>
+            <MainNavigation />
+            <Switch>
+                <Route path="/" exact>
+                    <Redirect to="/quotes" />
+                </Route>
+                <Route path="/quotes" exact>
+                    <AllQuotes />
+                </Route>
+                <Route path="/quotes/:quoteId">
+                    <QuoteDetail />
+                </Route>
+                <Route path="/new-quote">
+                    <NewQuote />
+                </Route>
+            </Switch>
+        </Fragment>
     );
 }
 
