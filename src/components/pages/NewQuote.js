5b92520ebcf5a2a786e6ca59227459948712bf48
berkagmpp <berkagmpp@gmail.com>
2022-06-08 11:35:38 +1200

    Feat: Add an object for sendRequest, status using useHTTP and addQuote components Fix typo FIREBASE_DOMAIN in api.js Add an object for sendRequest, status using useHTTP and addQuote components for sending a request

--- src/components/pages/NewQuote.js
+++ src/components/pages/NewQuote.js
@@ -1,12 +1,16 @@
 import { useHistory } from "react-router-dom";
 
 import QuoteForm from "../quotes/QuoteForm";
+import useHttp from "../../hooks/use-http";
+import { addQuote } from "../../lib/api";
 
 const NewQuote = () => {
+    const { sendRequest, status } = useHttp(addQuote);  // destructuring data
     const history = useHistory();
 
     const addQuoteHandler = quoteData => {
-        console.log(quoteData);
+        // console.log(quoteData);
+        sendRequest(quoteData);
 
         history.push('/quotes');
     };
