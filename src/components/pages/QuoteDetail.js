fc3e21394bd495301e721e5372f0e2b2f6dccd4a
berkagmpp <berkagmpp@gmail.com>
2022-06-09 10:13:19 +1200

    Feat: Add sending an HTTP request useEffect in QuoteDetail.js Add sending an HTTP request for real-time API of Firebase Delete DUMMY_DATA De-structuring quoteId from params(useParams) for getSingleQuote fn needs only quoteId Add JSX codes for showing status conditions with LoadingSpinner and NoQuotesFound component Ref: #d4506d633f13041f3d8f305d2bcb63665bdc71d6 Ref: #5b92520ebcf5a2a786e6ca59227459948712bf48

--- src/components/pages/QuoteDetail.js
+++ src/components/pages/QuoteDetail.js
@@ -1,37 +1,45 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 
+import useHttp from "../../hooks/use-http";
+import { getSingleQuote } from "../../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
 import HighlightedQuote from "../quotes/HighlightedQuote";
 import Comments from '../comments/Comments'
 
-const DUMMY_QUOTES = [
-    { 
-        id: 'q1',
-        author: 'Claire',
-        text: 'React-router practice'
-    },
-    { 
-        id: 'q2',
-        author: 'Derek',
-        text: 'React practice'
-    },
-];
-
 const QuoteDetail = () => {
     const params = useParams();
+    const { quoteId } = params;     // getSingleQuote fn needs only quoteId, so choose quoteId only from params 
+
     const match = useRouteMatch();
 
-    const quote = DUMMY_QUOTES.find(
-        (quote) => quote.id === params.quoteId
-    );
+    const { sendRequest, status, data: loadedQuote, error } = useHttp(getSingleQuote, true);
 
-    if (!quote) {
+    useEffect(()=>{
+        sendRequest(quoteId);
+    }, [sendRequest, quoteId]);     // quoteId which is destructed from params makes to useEffect re-run as precise as possible 
+    
+    if (status === "pending") {
+        return (
+            <div className='centered'>
+                <LoadingSpinner />
+            </div>
+        );
+    }
+
+    if (error) {
+        return (
+            <p className='centered focused'>{error}</p>
+        );
+    }
+
+    if (!loadedQuote.text) {    //  becouse loadedQuote is always returned from getSingleQuote fn of api.js, '.text' is necessary
         return <p>No quote found!</p>;
     }
 
     return (
         <Fragment>
-            <HighlightedQuote author={quote.author} text={quote.text} />
+            <HighlightedQuote author={loadedQuote.author} text={loadedQuote.text} />
             <Route path={match.path} exact> 
                 <div className='centered'>
                     <Link className='btn--flat' to={`${match.url}/comments`}>  
@@ -46,4 +54,4 @@ const QuoteDetail = () => {
     );
 };
 
-export default QuoteDetail;
+export default QuoteDetail; 
