d4506d633f13041f3d8f305d2bcb63665bdc71d6
berkagmpp <berkagmpp@gmail.com>
2022-06-09 09:42:39 +1200

    Feat: Add sending an HTTP request useEffect in AllQuotes.js Add sending an HTTP request for real-time API of Firebase Add JSX codes for showing status conditions with LoadingSpinner and NoQuotesFound component Ref: #5b92520ebcf5a2a786e6ca59227459948712bf48

--- src/components/pages/AllQuotes.js
+++ src/components/pages/AllQuotes.js
@@ -1,21 +1,38 @@
-import QuoteList from '../quotes/QuoteList';
+import { useEffect } from 'react';
 
-const DUMMY_QUOTES = [
-    { 
-        id: 'q1',
-        author: 'Claire',
-        text: 'React-router practice'
-    },
-    { 
-        id: 'q2',
-        author: 'Derek',
-        text: 'React practice'
-    },
-];
+import useHttp from '../../hooks/use-http';
+import { getAllQuotes } from '../../lib/api';
+import QuoteList from '../quotes/QuoteList';
+import LoadingSpinner from '../UI/LoadingSpinner';
+import NoQuotesFound from '../quotes/NoQuotesFound';
 
 const AllQuotes = () => {
+    const { sendRequest, status, data, error } = useHttp(getAllQuotes, true);    // destructuring data
+
+    useEffect(() => {
+        sendRequest();
+    }, [sendRequest]);
+
+    if (status === "pending") {
+        return (
+            <div className='centered'>
+                <LoadingSpinner />
+            </div>
+        );
+    }
+
+    if (error) {
+        return (
+            <p className='centered focused'>{error}</p>
+        );
+    }
+
+    if (status === "completed" && (!data || data.length === 0)) {
+        return <NoQuotesFound className='focused' />;
+    }
+
     return (
-        <QuoteList quotes={DUMMY_QUOTES} />
+        <QuoteList quotes={data} />
     );
 };
 
