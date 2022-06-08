c991100428db1908caab0a73ae4908548213dd3c
berkagmpp <berkagmpp@gmail.com>
2022-06-09 11:42:16 +1200

    Feat: Add sending an HTTP request useEffect in Commentsl.js Add sending an HTTP request for real-time API of Firebase: (send de-structuring quoteId from params(useParams) and onAddedComment from NewCommentForm component using getAllComments of api.js) Add codes for showing status conditions using 'comments' variable value changing Ref: #8a409bf9619ad37642989c0bf40e2ccaca7142ad Ref: #d4506d633f13041f3d8f305d2bcb63665bdc71d6

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,14 +1,49 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import useHttp from '../../hooks/use-http';
+import { getAllComments } from '../../lib/api';
+import NewCommentForm from './NewCommentForm';
+import LoadingSpinner from '../UI/LoadingSpinner';
+import CommentsList from './CommentsList'
 
 import classes from './Comments.module.css';
-import NewCommentForm from './NewCommentForm';
 
 const Comments = () => {
     const [isAddingComment, setIsAddingComment] = useState(false);
 
+    const params = useParams();
+    const { quoteId } = params;     // etAllComments fn needs only quoteId, so choose quoteId only from params 
+
+    const { sendRequest, status, data: loadedComments } = useHttp(getAllComments, true);
+
+    useEffect(() => {
+        sendRequest(quoteId);
+    }, [sendRequest, quoteId]);  // quoteId which is destructed from params makes to useEffect re-run as precise as possible 
+
     const startAddCommentHandler = () => {
         setIsAddingComment(true);
     };
+
+    const addedCommentHandler = () => {};
+
+    let comments;
+
+    if (status === "pending") {
+        comments = (
+            <div className='centered'>
+                <LoadingSpinner />
+            </div>
+        );
+    }
+
+    if (status === "completed" && (loadedComments && loadedComments.length > 0)) {
+        comments = <CommentsList comments={loadedComments} />
+    }
+
+    if (status === "completed" && (!loadedComments || loadedComments.length === 0)) {
+        comments = <p className='centered'>No comments were added yet!</p>;
+    }
 
     return (
         <section className={classes.comments}>
@@ -18,8 +53,8 @@ const Comments = () => {
                     Add a Comment
                 </button>
             )}
-            {isAddingComment && <NewCommentForm />}
-            <p>Comments...</p>
+            {isAddingComment && <NewCommentForm quoteId={quoteId} onAddedComment={addedCommentHandler} />}
+            {comments}
         </section>
     );
 };
